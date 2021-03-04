2b4d9bfbf19afc552e3726802dee872a1be7efff
DerekYZ <79599800+DerekYZ@users.noreply.github.com>
2021-03-03 17:58:50 -0800

    Update Temperature unit CFK Converting.ps1
    Improved logic flow. Using while loop to check unit and value range. Edit out unnecessary lines like set variable for value check, and for array setting.

--- Temperature unit CFK Converting.ps1
+++ Temperature unit CFK Converting.ps1
@@ -1,42 +1,32 @@
 #Convert Kelvin, Farhenheit and Celsius
 
 #check input unit other than k/f/c
-do {[string]$unit = Read-Host "Please provide Unit of measurement (C, K or F):>"}
-while ($unit -ne "k" -and $unit -ne "f" -and $unit -ne "c") 
-#set limit
-[float]$limitk = 0
-[float]$limitf = -459.7
-[float]$limitc = -273.16
-[float]$value = read-host "Please provide value for measurement:>"
+[string]$unit = Read-Host "Please provide Unit of measurement (C, K or F)"
+while ($unit -ne "k" -and $unit -ne "f" -and $unit -ne "c") {[string]$unit = Read-Host "Invalid unit, must be C/K/F"}
+[float]$value = read-host "Please provide value for measurement"
 #converting
-switch ($unit) {
-    {($_ -eq "k") -and ($value -ge $limitk)} {
-        $kelvin = $value;
-        $farhenheit = ($value-273.15)*9/5+32;
-        $celsius = $value-273.15;
+switch ($unit) { 
+    k { while ($value -lt [float]0) {[float]$value = read-host "Invalid Kelvin value, minimum 0"}
+        $result = @( [pscustomobject]@{
+            Kelvin = [math]::Round($value,2);
+            Farhenheit = [math]::Round(($value-273.15)*9/5+32,2);
+            Celsius = [math]::Round($value-273.15,2);
+        })
     }
-    {($_ -eq "f") -and ($value -ge $limitf)} {
-        $kelvin = ($value-32)*5/9+273.15;
-        $farhenheit = $value;
-        $celsius = ($value-32)*5/9;
+    f { while ($value -lt [float]-459.7) {[float]$value = read-host "Invalid Farhenheit value, minimum -459.7"}
+        $result = @( [pscustomobject]@{
+            Kelvin = [math]::Round(($value-32)*5/9+273.15,2);
+            Farhenheit = [math]::Round($value,2);
+            Celsius = [math]::Round(($value-32)*5/9,2);
+        })
     }
-    {($_ -eq "c") -and ($value -ge $limitc)} {
-        $kelvin = $value+273.15;
-        $farhenheit = $value*9/5+32;
-        $celsius = $value;   
-    }
-    default {
-    write-host ("Invalid input value, minimum 0 K/ -273.16 C/ -459.7 F. Your input:"+$value+$unit);
-    exit;
+    c { while ($value -lt [float]-273.15) {[float]$value = read-host "Invalid Celsius value, minimum -273.15"}
+        $result = @( [pscustomobject]@{
+            Kelvin = [math]::Round($value+273.15,2);
+            Farhenheit = [math]::Round($value*9/5+32,2);
+            Celsius = [math]::Round($value,2);
+        })
     }
 }
-#send value to array
-$result = @( 
-    [pscustomobject]@{
-        Kelvin = [math]::Round($kelvin,2);
-        Farhenheit = [math]::Round($farhenheit,2);
-        Celsius = [math]::Round($celsius,2);
-        }
-    )
 #display array
 $result
